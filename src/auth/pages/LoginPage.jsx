3c491d7a165507b120bbb2f5d535b6f5c274d175
Ignacio <Ignacio.todo@uai.cl>
2024-04-22 10:58:11 -0400

    route provate

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,28 +1,35 @@
-import { useNavigate } from "react-router-dom"
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 export const LoginPage = () => {
 
+  const { login } = useContext( AuthContext );
   const navigate = useNavigate();
 
   const onLogin = () => {
-    navigate('/',{
+    
+    const lastPath = localStorage.getItem('lastPath') || '/';
+
+    login( 'Ignacio Toro' );
+    
+    navigate( lastPath, {
       replace: true
-    })
+    });
   }
 
   return (
-    <>
-      <div className="container">
-        <h1>Login</h1>
-        <hr />
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr />
 
-        <button className="btn btn-primary" onClick={ onLogin }>
-          Login
-        </button>
+      <button 
+        className="btn btn-primary"
+        onClick={ onLogin }
+      >
+        Login
+      </button>
 
-      </div>
-
-    
-    </>
+    </div>
   )
 }
